d248bacedb79ef11db79848a9614731031c00113
TheLevic <levicrider08@gmail.com>
2021-09-09 10:22:38 -0500

    Possible solution to the second function

--- helper.cpp
+++ helper.cpp
@@ -20,6 +20,18 @@ bool is_the_char_in_the_set(char c, const char *set){
 }
 
 char *find_first_in_the_string_that_is_not_in_the_set(char *str, const char *set){
+    char *temp = NULL;
+    
+    while (*str != '\0'){ //Making sure we haven't hit the NULL character while looping in str
+        while (*set != '\0'){ //Making sure we haven't hit the NULL character while looping in set
+            if (*str != *set){
+                *temp = *str;
+            }
+            *set++;
+        }
+        *str++;
+    }
+return temp;
 }
 
 
