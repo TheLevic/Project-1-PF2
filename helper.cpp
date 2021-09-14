89f24e7b9ff28bd994eef11319693ba8bc758142
TheLevic <levicrider08@gmail.com>
2021-09-13 21:44:26 -0500

    Final

--- helper.cpp
+++ helper.cpp
@@ -21,11 +21,12 @@ bool is_the_char_in_the_set(char c, const char *set){
 
 char *find_first_in_the_string_that_is_not_in_the_set(char *str, const char *set){
     while (*str != '\0'){ //Making sure we haven't hit the NULL character while looping in str
-        while (*set != '\0'){ //Making sure we haven't hit the NULL character while looping in set
-            if (*str != *set){
+        const char *current = set;
+        while (*current != '\0'){ //Making sure we haven't hit the NULL character while looping in set
+            if (*str != *current){
                 return str;
             }
-            set++;
+            current++;
         }
         str++;
     }
--- homework1.cpp
+++ homework1.cpp
@@ -3,8 +3,8 @@ int main(){
     char test_string[15];
     char test_set[10];
     char *p; // pointer to the return character
-    strcpy(test_string, "Hi Everyone!");
-    strcpy(test_set, "e");
+    strcpy(test_string, "hot");
+    strcpy(test_set, "big");
     char x = 'h';
     if ( is_the_char_in_the_set(x, test_string) )
         cout << x << " appears in the following string: " << test_string << endl;
@@ -12,8 +12,19 @@ int main(){
         cout << x << " does not appear in the following string: " << test_string << endl;
     //the output is h does not appear in the following string: Hi Everyone!
     p = find_first_in_the_string_that_is_in_the_set(test_string, test_set);
-    cout << *p << endl;
-    // the above output should e and *(p-1) is v and *(p+1) is r
+    if (p != NULL){
+        cout << "The first char that is in the set is: " << *p << endl;
+        // the above output should e and *(p-1) is v and *(p+1) is r
+    }
+    else if (p == NULL){
+        cout << "NULL" << endl;
+    }
+
     p = find_first_in_the_string_that_is_not_in_the_set(test_string, test_set);
-    cout << *p << endl; // the output should be H
+    if (p != NULL){
+    cout << "The first char that is not in the set is: " <<  *p << endl; // the output should be H
+    }
+    else if (p == NULL){
+        cout << "NULL" << endl;
+    }
 }
